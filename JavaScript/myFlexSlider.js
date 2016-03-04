fe0fe1c71a9009d64a582b883a2bc900e8780818
JonasMalik <jonas14550@gmail.com>
2016-03-04 21:20:26 +0100

    flexslider almost done

---
+++ JavaScript/myFlexSlider.js
@@ -0,0 +1,8 @@
+$(window).load(function() {
+          $('.flexslider').flexslider({
+              animation: "slide",
+              animationLoop: false,
+              itemWidth: 310,
+              itemMargin: 5
+          });
+      });

ba99dcd5853916cfe1ca8e2ad58571a51e2b6dcf
Henrik <henrikchiang@gmail.com>
2016-03-10 15:28:58 +0100

    forgot to upload js file

--- JavaScript/mywebfunction.js
+++ JavaScript/mywebfunction.js
@@ -1,7 +1,44 @@
 var previous = 1;
-function setPortionSize(tallrikar) {
+function setPortionSizeforHenrik(tallrikar) {
     PortionSize = document.getElementsByClassName("setSize");
     for(var i = 0; i<11; i++){ 
+    var newPortionSize = parseFloat(PortionSize[i].innerHTML);
+    var twoPortion = (tallrikar * newPortionSize)/previous;  
+    PortionSize[i].innerHTML = twoPortion;     
+    }
+    previous = tallrikar;
+}
+
+function setPortionSizeforJuan(tallrikar) {
+    PortionSize = document.getElementsByClassName("setSize");
+    for(var i = 0; i<12; i++){ 
+    var newPortionSize = parseFloat(PortionSize[i].innerHTML);
+    var twoPortion = (tallrikar * newPortionSize)/previous;  
+    PortionSize[i].innerHTML = twoPortion;     
+    }
+    previous = tallrikar;
+}
+function setPortionSizeforJonas(tallrikar) {
+    PortionSize = document.getElementsByClassName("setSize");
+    for(var i = 0; i<6; i++){ 
+    var newPortionSize = parseFloat(PortionSize[i].innerHTML);
+    var twoPortion = (tallrikar * newPortionSize)/previous;  
+    PortionSize[i].innerHTML = twoPortion;     
+    }
+    previous = tallrikar;
+}
+function setPortionSizeforChristos(tallrikar) {
+    PortionSize = document.getElementsByClassName("setSize");
+    for(var i = 0; i<6; i++){ 
+    var newPortionSize = parseFloat(PortionSize[i].innerHTML);
+    var twoPortion = (tallrikar * newPortionSize)/previous;  
+    PortionSize[i].innerHTML = twoPortion;     
+    }
+    previous = tallrikar;
+}
+function setPortionSizeforAnton(tallrikar) {
+    PortionSize = document.getElementsByClassName("setSize");
+    for(var i = 0; i<6; i++){ 
     var newPortionSize = parseFloat(PortionSize[i].innerHTML);
     var twoPortion = (tallrikar * newPortionSize)/previous;  
     PortionSize[i].innerHTML = twoPortion;     
